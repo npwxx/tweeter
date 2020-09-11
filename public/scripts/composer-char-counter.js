02b6112f26152273e9ae2a523e256f79fe169933
Nicole Woodcock <nicole.xox@gmail.com>
2020-09-11 03:01:10 +0000

    add responsive design

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  $("#tweet-text").on("keyup", function() {
-    let text = event.target.value.length;
+  $("#tweet-text").on("keyup", function(e) {
+    let text = e.target.value.length;
     let remainingText = 140 - text;
     $("#counter").val(remainingText + "/140");
     if (remainingText < 0) {
